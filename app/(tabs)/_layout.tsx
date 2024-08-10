ddc8b42315770b49bffd38dd27ed385af1a3abba
chinmaynawkar <cnawkar19@gmail.com>
2024-08-10 17:19:40 +0530

    feat:added TabBarIcon component and BOTTOM TABS

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,87 @@
+/**
+ * This file is the main layout file for the app. It contains the Tabs component and the screens for each tab.
+ * The Tabs component is used to navigate between screens and the screens are defined in the app/(tabs) folder.
+ * Each screen has a corresponding file in the app/(tabs) folder.
+ */
+
 import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import TabBarIcon from '@/components/navigation/TabBarIcon';
+import icons from '@/constants/icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#FFA001',
+        tabBarInactiveTintColor: '#9BA1A6',
+        tabBarStyle: {
+          backgroundColor: '#161622',
+          borderTopWidth: 1,
+          borderTopColor: '#232533',
+          height: 84,
+        },
+
       }}>
-      <Tabs.Screen
-        name="index"
+      <Tabs.Screen 
+        name="home" 
         options={{
           title: 'Home',
+          headerShown: false,
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon
+              icon={icons.home}
+              color={color}
+              name='Home'
+              focused={focused}
+            />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="bookmark"
         options={{
-          title: 'Explore',
+          title: 'Bookmark',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon 
+              icon={icons.bookmark}
+              color={color}
+              name='Bookmark'
+              focused={focused}
+            />
           ),
         }}
       />
+      <Tabs.Screen
+         name='create'
+         options={{
+           title: 'Create',
+           tabBarIcon: ({ color, focused }) => (
+             <TabBarIcon 
+               icon={icons.plus}
+               color={color}
+               name='Create'
+               focused={focused}
+             />
+           ),
+         }}
+       />
+       <Tabs.Screen
+         name='profile'
+         options={{
+           title: 'Profile',
+           tabBarIcon: ({ color, focused }) => (
+             <TabBarIcon 
+               icon={icons.profile}
+               color={color}
+               name='Profile'
+               focused={focused}
+             />
+           ),
+         }}
+       />
+      
     </Tabs>
   );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,70 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
-
+import pluginReactNative from "eslint-plugin-react-native";
+import pluginPrettier from "eslint-plugin-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    env: {
+      es2021: true,
+      node: true,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react': pluginReact,
+      'react-native': pluginReactNative,
+      'prettier': pluginPrettier,
+      'pluginJs': pluginJs,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-native/all',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'no-debugger': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/prop-types': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+      'react-native/no-inline-styles': 'warn',
+      'react-native/no-unused-styles': 'warn',
+      'react-native/split-platform-components': 'warn',
+      'react-native/no-color-literals': 'error',
+      'react-native/no-raw-text': 'error',
+      'react-native/sort-styles': [
+        'error',
+        'asc',
+        {
+          ignoreClassNames: false,
+          ignoreStyleProperties: false,
+        },
+      ],
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ];
