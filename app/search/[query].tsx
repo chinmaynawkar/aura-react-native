33f41b1e71f4b2d53322e5584e73d153f9f2fa19
chinmaynawkar <cnawkar19@gmail.com>
2024-08-17 12:45:09 +0530

    feat:added search functionality & its screen

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,12 +1,63 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { FlatList } from 'react-native'
+import SearchInput from '@/components/SearchInput';
+import EmptyState from '@/components/EmptyState';
+import { searchVideos } from '@/lib';
+import useAppwrite from '@/hooks/useAppwrite';
+import VideoCard from '@/components/VideoCard';
+import { useLocalSearchParams } from 'expo-router';
 
-const Search = () => {
+const Search: React.FC = () => { 
+  const { query } = useLocalSearchParams<{ query: string }>();
+  // he following line is used to get the search query from the URL
+  const { data: posts, refetch } = useAppwrite(
+    () => searchVideos(query)
+  );
+
+  // console.log(JSON.stringify(query, null, 2));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Results
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">
+              {query}
+            </Text>
+            <View className="mt-6 mb-8">
+              <SearchInput intialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Search
+export default Search;
